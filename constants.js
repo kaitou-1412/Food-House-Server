c9d433525295675a623e319d3cbf2d70b00df1bf
Ruban Sahoo <rubansahoo1@gmail.com>
2024-01-14 01:02:52 +0530

    food-house server is ready

---
+++ constants.js
@@ -0,0 +1,4 @@
+export const FETCH_RESTAURANTS_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9083215&lng=77.6050777&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const FETCH_MENU_URL =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9083215&lng=77.6050777&restaurantId=";
--- server.js
+++ server.js
@@ -1,10 +1,70 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import fetch from "cross-fetch";
+import { FETCH_RESTAURANTS_URL, FETCH_MENU_URL } from "./constants.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+// For Restaurants List API
+app.get("/api/restaurants", async (req, res) => {
+  await fetch(FETCH_RESTAURANTS_URL, {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("An error occurred");
+    });
+});
+
+// For Menu API
+app.get("/api/menu", async (req, res) => {
+  const { restaurantID } = req.query;
+  console.log(req.query);
+
+  await fetch(FETCH_MENU_URL + restaurantID, {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("An error occurred");
+    });
+});
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my server!");
+  res.json({ data: "Welcome to my server!" });
 });
 
 app.listen(port, () => {
